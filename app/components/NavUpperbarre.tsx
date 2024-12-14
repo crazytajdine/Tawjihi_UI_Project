6aac029d2590e465c83cf07f8771fe09a690f5c7
Taj Eddine Marmoul <tajdinetajdine1@gmail.com>
2024-12-14 16:56:14 +0100

    main bar

--- app/components/NavUpperbarre.tsx
+++ app/components/NavUpperbarre.tsx
@@ -1,54 +1,67 @@
 import React from "react";
 import logo3 from "/public/logo-3.svg";
 import Image from "next/image";
-
-
+import Link from "next/link";
 
 export const NavUpperbarre = () => {
   return (
     <div
       className={`flex h-[46px] items-center justify-between px-9 py-3.5 relative w-full `}
     >
-      <div className="flex  relative w-[161.75px] ">
-        <Image
-          className=" w-7 h-[25px] "
-          alt="Logo"
-          src={logo3}
-        />
+      <Link href={"/"} className="flex  relative w-[161.75px] ">
+        <Image className=" w-7 h-[25px] " alt="Logo" src={logo3} />
 
         <div className=" w-[130px]  [font-family:'Preahvihear-Regular',Helvetica] font-normal text-m-3black text-[20.2px] tracking-[0] leading-[normal]">
           AFTERBAC
         </div>
-      </div>
+      </Link>
 
       <div className="flex w-[407.81px] items-center gap-[27px]  relative ">
-        <div className="relative   [font-family:'Georgia-Regular',Helvetica] font-normal text-m-3black text-lg tracking-[0] leading-[normal]">
+        <Link
+          href={"/"}
+          className="relative   [font-family:'Georgia-Regular',Helvetica] font-normal text-m-3black text-lg tracking-[0] leading-[normal]"
+        >
           Universités
-        </div>
+        </Link>
 
-        <div className="relative w-[100px]  [font-family:'Georgia-Regular',Helvetica] font-normal text-m-3black text-lg tracking-[0] leading-[normal]">
+        <Link
+          href={"/Specialites"}
+          className="relative w-[100px]  [font-family:'Georgia-Regular',Helvetica] font-normal text-m-3black text-lg tracking-[0] leading-[normal]"
+        >
           Spécialités
-        </div>
+        </Link>
 
-        <div className="relative w-[100px]  [font-family:'Georgia-Regular',Helvetica] font-normal text-m-3black text-lg tracking-[0] leading-[normal]">
+        <Link
+          href={"/Calender"}
+          className="relative w-[100px]  [font-family:'Georgia-Regular',Helvetica] font-normal text-m-3black text-lg tracking-[0] leading-[normal]"
+        >
           Calendrier
-        </div>
+        </Link>
 
-        <div className="relative w-[100px]  [font-family:'Georgia-Regular',Helvetica] font-normal text-m-3black text-lg tracking-[0] leading-[normal]">
+        <Link
+          href={"/conseils"}
+          className="relative w-[100px]  [font-family:'Georgia-Regular',Helvetica] font-normal text-m-3black text-lg tracking-[0] leading-[normal]"
+        >
           Conseils
-        </div>
+        </Link>
       </div>
 
       <div className="inline-flex items-center gap-[13.5px] relative flex-[0_0_auto] mt-[-14.62px] mb-[-14.62px]">
-        <div className="relative w-fit [font-family:'Georgia-Regular',Helvetica] font-normal text-m-3black text-lg tracking-[0] leading-[normal] whitespace-nowrap">
+        <Link
+          href={"/login"}
+          className="relative w-fit [font-family:'Georgia-Regular',Helvetica] font-normal text-m-3black text-lg tracking-[0] leading-[normal] whitespace-nowrap"
+        >
           Se connecter
-        </div>
+        </Link>
 
-        <div className="inline-flex items-center justify-center gap-[5.62px] p-[13.5px] relative flex-[0_0_auto] bg-collection-1-secondary rounded-[9px]">
+        <Link
+          href={"/register"}
+          className="inline-flex items-center justify-center gap-[5.62px] p-[13.5px] relative flex-[0_0_auto] bg-collection-1-secondary rounded-[9px]"
+        >
           <div className="relative w-fit  [font-family:'Georgia-Regular',Helvetica] font-normal text-collection-1-white text-lg tracking-[0] leading-[normal] whitespace-nowrap">
             S’inscrire
           </div>
-        </div>
+        </Link>
       </div>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import { NavMain } from "./components/Navmain";
+import { NavMain } from "./components/main_Components/TableSearch/Navmain";
 import { NavUpperbarre } from "./components/NavUpperbarre";
 
 export default function Home() {
   return (
     <div>
-    <NavUpperbarre />
-    <NavMain />
+      <NavUpperbarre />
+      <NavMain />
     </div>
   );
 }
